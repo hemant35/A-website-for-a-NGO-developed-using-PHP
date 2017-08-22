55e7eba810661d656f0901cb785f5abb69e3f3ab
jaswan26 <jaswan26@gmail.com>
2017-08-23 00:43:59 +0530

    Create footer.php

---
+++ inc/footer.php
@@ -0,0 +1,36 @@
+ <footer>
+
+      <div class="row">
+
+         <div class="twelve columns">
+
+            <ul class="footer-nav">
+					<li><a href="#">Home.</a></li>
+              	<li><a href="contact.html">About us</a></li>
+              	<li><a href="#">Campaign</a></li>
+              	<li><a href="#">Get Involved</a></li>
+              	<li><a href="#">Contact us.</a></li>
+               <li><a href="#"></a></li>
+			   </ul>
+
+            <ul class="footer-social">
+               <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+               <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+               <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+               <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+               <li><a href="#"><i class="fa fa-skype"></i></a></li>
+               <li><a href="#"><i class="fa fa-rss"></i></a></li>
+            </ul>
+
+            <ul class="copyright">
+               <li>Copyright &copy; 2017 </li> 
+               <li>Design by <a href="mypage/index.html">	Jaswan26</a></li>               
+            </ul>
+
+         </div>
+
+         <div id="go-top" style="display: block;"><a title="Back to Top" href="#">Go To Top</a></div>
+
+      </div>
+
+   </footer> <!-- Footer End-->
