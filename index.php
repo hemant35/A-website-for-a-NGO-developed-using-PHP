c94f56000817b17a091558a71ab4097a38c9e1ea
hemant35 <31255382+hemant35@users.noreply.github.com>
2017-08-23 00:18:42 +0530

    index page added

---
+++ index.php
@@ -0,0 +1,223 @@
+<?php
+$currentPage=1;
+ include("inc/header.php"); ?>
+  <!-- Intro Section
+   ================================================== -->
+   <section id="intro">
+
+      <!-- Flexslider Start-->
+	   <div id="intro-slider" class="flexslider">
+
+		   <ul class="slides">
+
+			   <!-- Slide -->
+			   <li>
+				   <div class="row">
+					   <div class="twelve columns">
+						   <div class="slider-text">
+							   <h1>Education<span>.</span></h1>
+							   <p>Aenean condimentum, lacus sit amet luctus lobortis, dolores et quas molestias excepturi
+                        enim tellus ultrices elit, amet consequat enim elit noneas sit amet luctu. lacus sit amet luctus lobortis, dolores et quas molestias excepturi
+                        enim tellus ultrices elit.</p>
+						   </div>
+                     <div class="slider-image">
+                        <img src="images/sliders/index1-banner-me.jpg" alt="" />
+                     </div>
+					   </div>
+				   </div>
+			   </li>
+
+            <!-- Slide -->
+			   <li>
+				   <div class="row">
+					   <div class="twelve columns">
+						   <div class="slider-text">
+							   <h1>Health<span>.</span></h1>
+							   <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+                        deleniti eos et accusamus. amet consequat enim elit noneas sit amet luctu. lacus sit amet luctus lobortis.
+                        Aenean condimentum, lacus sit amet luctus.</p>
+						   </div>
+                     <div class="slider-image">
+                        <img src="images/sliders/index2-banner-me.jpg" alt="" />
+                     </div>
+					   </div>
+				   </div>
+			   </li>
+
+		   </ul>
+
+	   </div> <!-- Flexslider End-->
+	   
+   </section> <!-- Intro Section End-->
+
+   <!-- Info Section
+   ================================================== -->
+   <section id="info">
+
+      <div class="row">
+
+         <div class="bgrid-quarters s-bgrid-halves">
+
+           <div class="columns">
+              <h2>Clean & Modern India.</h2>
+
+              <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+              Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              </p>
+           </div>
+
+           <div class="columns">
+              <h2>Disaster response.</h2>
+
+              <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+              Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              </p>
+           </div>
+
+           <div class="columns s-first">
+              <h2>Healthy India.</h2>
+
+              <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+              Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              </p>
+           </div>
+
+           <div class="columns">
+              <h2>Free Service.</h2>
+
+              <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+              Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              </p>
+           </div>
+
+           </div>
+
+      </div>
+
+   </section> <!-- Info Section End-->
+
+   <!-- Works Section
+   ================================================== -->
+   <section id="works">
+
+      <div class="row">
+
+         <div class="twelve columns align-center">
+            <h1>Some of our recently started services.</h1>
+         </div>
+
+         <div id="portfolio-wrapper" class="bgrid-quarters s-bgrid-halves">
+
+    		   <div class="columns portfolio-item">
+               <div class="item-wrap">
+    				   <a href="portfolio.html">
+                     <img alt="" src="images/portfolio/geometrics1.jpg">
+                     <div class="overlay"></div>
+                     <div class="link-icon"><i class="fa fa-link"></i></div>
+                  </a>
+    					<div class="portfolio-item-meta">
+    					   <h5><a href="portfolio.html">Menuha.com</a></h5>
+                     <p>Price varies</p>
+    					</div>
+               </div>
+    			</div>
+
+            <div class="columns portfolio-item">
+               <div class="item-wrap">
+    				   <a href="portfolio.html">
+                     <img alt="" src="images/portfolio/console1.jpg">
+                     <div class="overlay"></div>
+                     <div class="link-icon"><i class="fa fa-link"></i></div>
+                  </a>
+    					<div class="portfolio-item-meta">
+    					   <h5><a href="portfolio.html">Donate4kishan</a></h5>
+                     <p>Free</p>
+    					</div>
+               </div>
+    			</div>
+
+            <div class="columns portfolio-item s-first">
+               <div class="item-wrap">
+    				   <a href="portfolio.html">
+                     <img alt="" src="images/portfolio/camera1.jpg">
+                     <div class="overlay"></div>
+                     <div class="link-icon"><i class="fa fa-link"></i></div>
+                  </a>
+    					<div class="portfolio-item-meta">
+    					   <h5><a href="portfolio.html">ANTI-HARASSMENT SQUAD</a></h5>
+                     <p>Price varies</p>
+    					</div>
+               </div>
+    			</div>
+
+            <div class="columns portfolio-item">
+               <div class="item-wrap">
+    				   <a href="portfolio.html">
+                     <img alt="" src="images/portfolio/eye1.jpg">
+                     <div class="overlay"></div>
+                     <div class="link-icon"><i class="fa fa-link"></i></div>
+                      </a>
+    					<div class="portfolio-item-meta">
+    					   <h5><a href="portfolio.html">"EYE" THE REVOLUTION</a></h5>
+                     <p>Free</p>
+    					</div>
+               </div>
+    			</div>
+
+         </div>
+
+      </div>
+
+   </section> <!-- Works Section End-->
+
+   <!-- Journal Section -->
+   <center>
+   <div class="fb-post" data-href="https://www.facebook.com/DFOWORLD/posts/1845068082474793" data-width="750" data-show-text="true"><blockquote cite="https://www.facebook.com/DFOWORLD/posts/1845068082474793" class="fb-xfbml-parse-ignore"><p>#MORAL_EDUCATION
+Character education is an umbrella term loosely used to describe the teaching of children in a manner...</p>Posted by <a href="https://www.facebook.com/DFOWORLD/">Diamond fantasies</a> on&nbsp;<a href="https://www.facebook.com/DFOWORLD/posts/1845068082474793">Sunday, 13 August 2017</a></blockquote></div>
+   </center>
+   <!-- Call-To-Action Section
+   ================================================== -->
+   <section id="call-to-action">
+
+      <div class="row">
+
+         <div class="eight columns offset-1">
+
+            <h1><a href="http://www.dreamhost.com/r.cgi?287326|STYLESHOUT">Get Involved.</a></h1>
+            <p>Looking for Helping others keep <a href="http://www.dreamhost.com/r.cgi?287326|STYLESHOUT"><span>Donating</span></a>.
+					Feel <span> Happy</span> By helping others with<span> Diamond Fantasies</span>. 
+					<!-- Simply type	the promocode in the box labeled “Promo Code” when placing your order. --></p>
+
+         </div>
+
+         <div class="three columns action">
+
+            <a href="http://www.dreamhost.com/r.cgi?287326|STYLESHOUT" class="button">Donate Now</a>
+
+         </div>
+
+      </div>
+
+   </section> <!-- Call-To-Action Section End-->
+
+ 
+
+
+
+   <!-- footer
+   ================================================== -->
+  <?php include("inc/footer.php"); ?>
+
+   <!-- Java Script
+   ================================================== -->
+   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+   <script>window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')</script>
+   <script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
+
+   <script src="js/jquery.flexslider.js"></script>
+   <script src="js/doubletaptogo.js"></script>
+   <script src="js/init.js"></script>
+
+</body>
+
+</html>
