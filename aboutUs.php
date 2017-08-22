2cd7c24d1640bcf846992eeaf27a4372413c86a8
jaswan26 <jaswan26@gmail.com>
2017-08-23 00:28:57 +0530

    conatct.php and aboutus.php pages are added

---
+++ aboutUs.php
@@ -0,0 +1,31 @@
+<?php 
+$currentPage=2;
+include("inc/header.php"); ?>
+<br></br>
+<br></br>
+<h1>Error (#512)</h1>
+
+    <div class="alert alert-danger">
+        An internal server error occurred.    </div>
+
+<p>
+    The error occurred while the Web server was processing your request.
+</p>
+<p>
+    Please contact us if you think this is a server error. Thank you.
+</p>        </div>
+
+ <?php include("inc/footer.php"); ?>
+  <!-- Java Script
+   ================================================== -->
+   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+   <script>window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')</script>
+   <script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
+
+   <script src="js/jquery.flexslider.js"></script>
+   <script src="js/doubletaptogo.js"></script>
+   <script src="js/init.js"></script>
+
+</body>
+
+</html>
